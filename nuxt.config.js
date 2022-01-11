03624ed53750b312f8fe0cc7ae09dd576c3aee7b
Chris <kispi@naver.com>
2022-01-11 15:28:49 +0900

    add app.html

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,9 +17,6 @@ const config = {
         link: [
             { rel: 'icon', href: '/favicon.png' }
         ],
-        script: [
-            { defer: true, hid: 'adsense', src: '/adsense.js' },
-        ],
     },
     router: {
         scrollBehavior: () => {
